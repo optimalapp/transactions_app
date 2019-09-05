9ffc3623a23e3f0803c46e844f4370ee3b911e64
Stas L <stan.lyashenko@gmail.com>
2019-09-05 15:34:15 +0300

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
   validates :name, presence: true
   validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def active?
     status == 'active'
